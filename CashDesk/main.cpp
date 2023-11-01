b55d9e1ef64fb7b17ce8eaafa73e90c252f3ae67
antdaddy <45079590+antdaddy@users.noreply.github.com>
2023-11-02 00:35:01 +0300

    Update main.cpp

--- CashDesk/main.cpp
+++ CashDesk/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
+69
  char name[20];
+ cout << "Hello world! Have a nice day!\n";
  cout << "Enter your name: ";
  cin >> name;
  cout << "Hello, " << name << "!\n";
  cout << "Nice to see you learning Git\n";
  system("pause");
  return 0;
-} 
+}
+
